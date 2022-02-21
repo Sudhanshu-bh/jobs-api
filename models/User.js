a741c92ec123bc54705eb6762249a9c3d46718d1
Sudhanshu <bsudhanshu3@gmail.com>
2022-02-21 23:17:23 +0530

    implemented password hashing and jwt

--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -20,8 +22,24 @@ const UserSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Password must be provided'],
     minlength: 6,
-    maxlength: 12,
   },
 });
+
+UserSchema.pre('save', async function () {
+  const salt = bcrypt.genSaltSync(10);
+  this.password = bcrypt.hashSync(this.password, salt);
+  // next();
+});
+
+UserSchema.methods.createJWT = function () {
+  const token = jwt.sign(
+    { userId: this._id, name: this.name },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: process.env.JWT_LIFETIME,
+    }
+  );
+  return token;
+};
 
 module.exports = mongoose.model('User', UserSchema);
